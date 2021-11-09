cd91e7fde951c8bb96ad9614be2ce0d01aa3c715
mi <mi5@mi5.sakura.ne.jp>
2021-11-10 00:54:06 +0900

    feat: 日本語キーボード環境でも英語キーボード配列で入力できるレイヤーを追加

--- keymap.c
+++ keymap.c
@@ -15,9 +15,12 @@
  */
 #include QMK_KEYBOARD_H
 #include "drivers/issi/is31fl3733.h"
+#include "keymap_jp.h"
 
 enum layers {
     _BASE = 0,
+    _JP,
+    _JP_SHIFT,
     _MAC,
     _VIM,
     _FN
@@ -33,10 +36,24 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT_65_ansi( /* Base */
     KC_GESC,          KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS,      KC_EQL,  KC_BSPC, TG(_MAC),\
-    KC_TAB,           KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC,      KC_RBRC, KC_BSLS, KC_PGUP, \
+    KC_TAB,           KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC,      KC_RBRC, KC_BSLS, TG(_JP), \
     LCTL_T(KC_LANG2), KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,               KC_ENT,  DISABLE_FORCE_IME_OFF, \
     LSFT_T(KC_LANG1), KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_GRV),        KC_UP,   MO(_FN), \
     MO(_VIM),         KC_LALT, KC_DEL,                 KC_SPC,                          KC_RCTL, KC_RALT, KC_RGUI,      KC_LEFT, KC_DOWN, KC_RGHT),
+
+[_JP] = LAYOUT_65_ansi( /* 106/109 */
+    KC_TRNS,                 JP_1,    JP_2,   JP_3,   JP_4,   JP_5,   JP_6,   JP_7,   JP_8,    JP_9,    JP_0,    JP_MINS, JP_EQL,  KC_TRNS, KC_TRNS,\
+    KC_TRNS,                 JP_Q,    JP_W,   JP_E,   JP_R,   JP_T,   JP_Y,   JP_U,   JP_I,    JP_O,    JP_P,    JP_LBRC, JP_RBRC, JP_BSLS, KC_TRNS,\
+    KC_TRNS,                 JP_A,    JP_S,   JP_D,   JP_F,   JP_G,   JP_H,   JP_J,   JP_K,    JP_L,    JP_SCLN, JP_QUOT,          KC_TRNS, KC_TRNS,\
+    LT(_JP_SHIFT, KC_LANG1), JP_Z,    JP_X,   JP_C,   JP_V,   JP_B,   JP_N,   JP_M,   JP_COMM, JP_DOT,  JP_SLSH, JP_GRV,           KC_TRNS, KC_TRNS,\
+    KC_TRNS,                 KC_TRNS, KC_TRNS,                KC_TRNS,                         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+
+[_JP_SHIFT] = LAYOUT_65_ansi( /* 106/109 Shift */
+    KC_TRNS, JP_EXLM, JP_AT,   JP_HASH, JP_DLR,  JP_PERC, JP_CIRC, JP_AMPR, JP_ASTR, JP_LPRN, JP_RPRN, JP_UNDS, JP_PLUS, KC_TRNS, KC_TRNS,\
+    KC_TRNS, S(JP_Q), S(JP_W), S(JP_E), S(JP_R), S(JP_T), S(JP_Y), S(JP_U), S(JP_I), S(JP_O), S(JP_P), JP_LCBR, JP_RCBR, JP_PIPE, KC_TRNS,\
+    KC_TRNS, S(JP_A), S(JP_S), S(JP_D), S(JP_F), S(JP_G), S(JP_H), S(JP_J), S(JP_K), S(JP_L), JP_COLN, JP_DQUO,          KC_TRNS, KC_TRNS,\
+    KC_TRNS, S(JP_Z), S(JP_X), S(JP_C), S(JP_V), S(JP_B), S(JP_N), S(JP_M), JP_LABK, JP_RABK, JP_QUES, JP_TILD,          KC_TRNS, KC_TRNS,\
+    KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
 [_MAC] = LAYOUT_65_ansi( /* Mac */
     KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,\
@@ -76,6 +93,22 @@ const uint16_t PROGMEM force_ime_off_shift_keycodes[] = {
 };
 const size_t force_ime_off_shift_keycodes_length = sizeof(force_ime_off_shift_keycodes) / sizeof(force_ime_off_shift_keycodes[0]);
 
+const uint16_t PROGMEM force_ime_off_jp_keycodes[] = {
+    JP_1, JP_2, JP_3, JP_4, JP_5, JP_6, JP_7, JP_8, JP_9, JP_0, JP_EQL,
+    JP_BSLS,
+    JP_SCLN, JP_QUOT,
+    JP_GRV
+};
+const size_t force_ime_off_jp_keycodes_length = sizeof(force_ime_off_jp_keycodes) / sizeof(force_ime_off_jp_keycodes[0]);
+
+const uint16_t PROGMEM force_ime_off_shift_jp_keycodes[] = {
+    JP_EXLM, JP_AT, JP_HASH, JP_DLR,JP_PERC, JP_CIRC, JP_AMPR, JP_ASTR, JP_LPRN, JP_RPRN, JP_UNDS, JP_PLUS,
+    S(JP_Q), S(JP_W), S(JP_E), S(JP_R), S(JP_T), S(JP_Y), S(JP_U), S(JP_I), S(JP_O), S(JP_P), JP_LCBR, JP_RCBR, JP_PIPE,
+    S(JP_A), S(JP_S), S(JP_D), S(JP_F), S(JP_G), S(JP_H), S(JP_J), S(JP_K), S(JP_L), JP_COLN, JP_DQUO,
+    S(JP_Z), S(JP_X), S(JP_C), S(JP_V), S(JP_B), S(JP_N), S(JP_M), JP_LABK, JP_RABK, JP_QUES, JP_TILD
+};
+const size_t force_ime_off_shift_jp_keycodes_length = sizeof(force_ime_off_shift_jp_keycodes) / sizeof(force_ime_off_shift_jp_keycodes[0]);
+
 bool enable_force_ime_off = true;
 
 void matrix_init_user(void) {
@@ -89,7 +122,21 @@ void matrix_scan_user(void) {
 void process_record_force_ime_off(uint16_t keycode, keyrecord_t *record) {
     bool force_ime_off = false;
 
-    if (0 < (get_mods() & MOD_MASK_SHIFT)) {
+    if (layer_state_is(_JP_SHIFT)) {
+        for (size_t i = 0; i < force_ime_off_shift_jp_keycodes_length; ++i) {
+            if (keycode == force_ime_off_shift_jp_keycodes[i]) {
+                force_ime_off = true;
+                break;
+            }
+        }
+    } else if (layer_state_is(_JP)) {
+        for (size_t i = 0; i < force_ime_off_jp_keycodes_length; ++i) {
+            if (keycode == force_ime_off_jp_keycodes[i]) {
+                force_ime_off = true;
+                break;
+            }
+        }
+    } else if (0 < (get_mods() & MOD_MASK_SHIFT)) {
         for (size_t i = 0; i < force_ime_off_shift_keycodes_length; ++i) {
             if (keycode == force_ime_off_shift_keycodes[i]) {
                 force_ime_off = true;
@@ -174,7 +221,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     uint8_t G = 0;
     uint8_t B = 0;
 
-    if (layer_state_cmp(state, _VIM)) {
+    if (layer_state_cmp(state, _JP)) {
         R = 255;
         B = 255;
     }
