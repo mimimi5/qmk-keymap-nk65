29e9ede8c61f94bac7a4b3617b2faaeeecadbeea
mi <mi5@mi5.sakura.ne.jp>
2022-02-05 22:00:13 +0900

    feat: 日本語配列でもUS配列対応をKey Overridesで実装

--- keymap.c
+++ keymap.c
@@ -19,8 +19,6 @@
 
 enum layers {
     _BASE = 0,
-    _JP,
-    _JP_SHIFT,
     _MAC,
     _VIM,
     _FN
@@ -33,27 +31,58 @@ enum custom_keycodes {
   VIM_WORD_BACK
 };
 
+const key_override_t at_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_2, JP_AT); // @
+const key_override_t circ_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_6, JP_CIRC); // ^
+const key_override_t ampr_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_7, JP_AMPR); // &
+const key_override_t astr_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_8, JP_ASTR); // *
+const key_override_t lprn_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_9, JP_LPRN); // (
+const key_override_t rprn_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_0, JP_RPRN); // )
+const key_override_t unds_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_MINS, JP_UNDS); // _
+const key_override_t plus_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_EQL, JP_PLUS); // +
+const key_override_t lcbr_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_LBRC, JP_LCBR); // {
+const key_override_t rcbr_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_RBRC, JP_RCBR); // }
+const key_override_t pipe_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSLS, JP_PIPE); // |
+const key_override_t coln_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_SCLN, JP_COLN); // :
+const key_override_t dquo_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_QUOT, JP_DQUO); // "
+const key_override_t tild_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_GRV, JP_TILD); // ~
+const key_override_t eql_key_override = ko_make_with_layers_and_negmods(0, KC_EQL, JP_EQL, ~0, (uint8_t) MOD_MASK_SHIFT); // =
+const key_override_t lbrc_key_override = ko_make_with_layers_and_negmods(0, KC_LBRC, JP_LBRC, ~0, (uint8_t) MOD_MASK_SHIFT); // [
+const key_override_t rbrc_key_override = ko_make_with_layers_and_negmods(0, KC_RBRC, JP_RBRC, ~0, (uint8_t) MOD_MASK_SHIFT); // ]
+const key_override_t bsls_key_override = ko_make_with_layers_and_negmods(0, KC_BSLS, JP_BSLS, ~0, (uint8_t) MOD_MASK_SHIFT); // backslash
+const key_override_t quot_key_override = ko_make_with_layers_and_negmods(0, KC_QUOT, JP_QUOT, ~0, (uint8_t) MOD_MASK_SHIFT); // '
+const key_override_t grv_key_override = ko_make_with_layers_and_negmods(0, KC_GRV, JP_GRV, ~0, (uint8_t) MOD_MASK_SHIFT); // `
+
+const key_override_t **key_overrides = (const key_override_t *[]){
+    &at_key_override,
+    &circ_key_override,
+    &ampr_key_override,
+    &astr_key_override,
+    &lprn_key_override,
+    &rprn_key_override,
+    &unds_key_override,
+    &plus_key_override,
+    &lcbr_key_override,
+    &rcbr_key_override,
+    &pipe_key_override,
+    &coln_key_override,
+    &dquo_key_override,
+    &tild_key_override,
+    &eql_key_override,
+    &lbrc_key_override,
+    &rbrc_key_override,
+    &bsls_key_override,
+    &quot_key_override,
+    &grv_key_override,
+    NULL
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT_65_ansi( /* Base */
     KC_GESC,          KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,   KC_0,    KC_MINS,      KC_EQL,  KC_BSPC, TG(_MAC),\
-    KC_TAB,           KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,   KC_P,    KC_LBRC,      KC_RBRC, KC_BSLS, TG(_JP), \
+    KC_TAB,           KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,   KC_P,    KC_LBRC,      KC_RBRC, KC_BSLS, KEY_OVERRIDE_TOGGLE, \
     LCTL_T(KC_LANG2), KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,   KC_SCLN, KC_QUOT,               KC_ENT,  DISABLE_FORCE_IME_OFF, \
     LSFT_T(KC_LANG1), KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_GRV),        KC_UP,   MO(_FN), \
     MO(_VIM),         KC_LALT, KC_DEL,                 KC_SPC,                          KC_ENT, KC_RALT, KC_RGUI,      KC_LEFT, KC_DOWN, KC_RGHT),
-
-[_JP] = LAYOUT_65_ansi( /* 106/109 */
-    KC_TRNS,                 JP_1,    JP_2,   JP_3,   JP_4,   JP_5,   JP_6,   JP_7,   JP_8,    JP_9,    JP_0,    JP_MINS, JP_EQL,  KC_TRNS, KC_TRNS,\
-    KC_TRNS,                 JP_Q,    JP_W,   JP_E,   JP_R,   JP_T,   JP_Y,   JP_U,   JP_I,    JP_O,    JP_P,    JP_LBRC, JP_RBRC, JP_BSLS, KC_TRNS,\
-    KC_TRNS,                 JP_A,    JP_S,   JP_D,   JP_F,   JP_G,   JP_H,   JP_J,   JP_K,    JP_L,    JP_SCLN, JP_QUOT,          KC_TRNS, KC_TRNS,\
-    LT(_JP_SHIFT, KC_LANG1), JP_Z,    JP_X,   JP_C,   JP_V,   JP_B,   JP_N,   JP_M,   JP_COMM, JP_DOT,  JP_SLSH, JP_GRV,           KC_TRNS, KC_TRNS,\
-    KC_TRNS,                 KC_TRNS, KC_TRNS,                KC_TRNS,                         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-
-[_JP_SHIFT] = LAYOUT_65_ansi( /* 106/109 Shift */
-    KC_TRNS,   JP_EXLM,   JP_AT,      JP_HASH, JP_DLR,  JP_PERC,   JP_CIRC, JP_AMPR, JP_ASTR, JP_LPRN,   JP_RPRN,    JP_UNDS,    JP_PLUS,    S(KC_BSPC), KC_TRNS,\
-    S(KC_TAB), S(JP_Q),   S(JP_W),    S(JP_E), S(JP_R), S(JP_T),   S(JP_Y), S(JP_U), S(JP_I), S(JP_O),   S(JP_P),    JP_LCBR,    JP_RCBR,    JP_PIPE,    KC_TRNS,\
-    KC_TRNS,   S(JP_A),   S(JP_S),    S(JP_D), S(JP_F), S(JP_G),   S(JP_H), S(JP_J), S(JP_K), S(JP_L),   JP_COLN,    JP_DQUO,                S(KC_ENT),  KC_TRNS,\
-    KC_TRNS,   S(JP_Z),   S(JP_X),    S(JP_C), S(JP_V), S(JP_B),   S(JP_N), S(JP_M), JP_LABK, JP_RABK,   JP_QUES,    JP_TILD,                S(KC_UP),   KC_TRNS,\
-    KC_TRNS,   S(KC_LALT), S(KC_DEL),                   S(KC_SPC),                            S(KC_ENT), S(KC_RGUI), S(KC_RGUI), S(KC_LEFT), S(KC_DOWN), S(KC_RGHT)),
 
 [_MAC] = LAYOUT_65_ansi( /* Mac */
     KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,\
@@ -93,26 +122,11 @@ const uint16_t PROGMEM force_ime_off_shift_keycodes[] = {
 };
 const size_t force_ime_off_shift_keycodes_length = sizeof(force_ime_off_shift_keycodes) / sizeof(force_ime_off_shift_keycodes[0]);
 
-const uint16_t PROGMEM force_ime_off_jp_keycodes[] = {
-    JP_1, JP_2, JP_3, JP_4, JP_5, JP_6, JP_7, JP_8, JP_9, JP_0, JP_EQL,
-    JP_BSLS,
-    JP_SCLN, JP_QUOT,
-    JP_GRV
-};
-const size_t force_ime_off_jp_keycodes_length = sizeof(force_ime_off_jp_keycodes) / sizeof(force_ime_off_jp_keycodes[0]);
-
-const uint16_t PROGMEM force_ime_off_shift_jp_keycodes[] = {
-    JP_EXLM, JP_AT, JP_HASH, JP_DLR,JP_PERC, JP_CIRC, JP_AMPR, JP_ASTR, JP_LPRN, JP_RPRN, JP_UNDS, JP_PLUS,
-    S(JP_Q), S(JP_W), S(JP_E), S(JP_R), S(JP_T), S(JP_Y), S(JP_U), S(JP_I), S(JP_O), S(JP_P), JP_LCBR, JP_RCBR, JP_PIPE,
-    S(JP_A), S(JP_S), S(JP_D), S(JP_F), S(JP_G), S(JP_H), S(JP_J), S(JP_K), S(JP_L), JP_COLN, JP_DQUO,
-    S(JP_Z), S(JP_X), S(JP_C), S(JP_V), S(JP_B), S(JP_N), S(JP_M), JP_LABK, JP_RABK, JP_QUES, JP_TILD
-};
-const size_t force_ime_off_shift_jp_keycodes_length = sizeof(force_ime_off_shift_jp_keycodes) / sizeof(force_ime_off_shift_jp_keycodes[0]);
-
 bool enable_force_ime_off = true;
 
 void matrix_init_user(void) {
   //user initialization
+  key_override_off();
 }
 
 void matrix_scan_user(void) {
@@ -122,21 +136,7 @@ void matrix_scan_user(void) {
 void process_record_force_ime_off(uint16_t keycode, keyrecord_t *record) {
     bool force_ime_off = false;
 
-    if (layer_state_is(_JP_SHIFT)) {
-        for (size_t i = 0; i < force_ime_off_shift_jp_keycodes_length; ++i) {
-            if (keycode == force_ime_off_shift_jp_keycodes[i]) {
-                force_ime_off = true;
-                break;
-            }
-        }
-    } else if (layer_state_is(_JP)) {
-        for (size_t i = 0; i < force_ime_off_jp_keycodes_length; ++i) {
-            if (keycode == force_ime_off_jp_keycodes[i]) {
-                force_ime_off = true;
-                break;
-            }
-        }
-    } else if (0 < (get_mods() & MOD_MASK_SHIFT)) {
+    if (0 < (get_mods() & MOD_MASK_SHIFT)) {
         for (size_t i = 0; i < force_ime_off_shift_keycodes_length; ++i) {
             if (keycode == force_ime_off_shift_keycodes[i]) {
                 force_ime_off = true;
@@ -179,11 +179,6 @@ bool register_and_unregister_code16(uint16_t mac_keycode, uint16_t keycode, keyr
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (keycode == DISABLE_FORCE_IME_OFF) {
-        enable_force_ime_off = !record->event.pressed;
-        return true;
-    }
-
     switch (keycode)
     {
     case DISABLE_FORCE_IME_OFF:
@@ -195,6 +190,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         return register_and_unregister_code16(A(KC_RGHT), C(KC_RGHT), record);
     case VIM_WORD_BACK:
         return register_and_unregister_code16(A(KC_LEFT), C(KC_LEFT), record);
+    case KEY_OVERRIDE_TOGGLE:
+        if (!record->event.pressed) {
+            IS31FL3733_set_color(7 + 64 - 1, 0, key_override_is_enabled() ? 255 : 0, 0);
+        }
+
+        return true;
     default:
         break;
     }
@@ -211,23 +212,17 @@ bool led_update_user(led_t led_state) {
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    if (layer_state_cmp(state, _FN)) {
-        IS31FL3733_set_color(7 + 64 - 1, 0, 255, 0);
-    } else {
-        IS31FL3733_set_color(7 + 64 - 1, 0, 0, 0);
-    }
-
     uint8_t R = 0;
     uint8_t G = 0;
     uint8_t B = 0;
 
-    if (layer_state_cmp(state, _JP)) {
-        R = 255;
-        B = 255;
-    }
-
     if (layer_state_cmp(state, _MAC)) {
         G = 255;
+    }
+
+    if (layer_state_cmp(state, _FN)) {
+        R = 255;
+        B = 255;
     }
 
     IS31FL3733_set_color(6 + 64 - 1, R, G, B);
